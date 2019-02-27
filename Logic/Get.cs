38faf50bcbec4ae529e758806869f0a7fd6966f6
Pontus Kjell <kjellpontus@gmail.com>
2019-02-27 13:33:35 +0100

    Add project files.

---
+++ Logic/Get.cs
@@ -0,0 +1,19 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic
+{
+   public class Get
+    {
+        public Get() { }
+
+        public Hello GetHello()
+        {
+            var hello = new Hello();
+            hello.Name = "Jesper";
+            return hello;
+        }
+    }
+}
---
+++ PCL/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Logic;
+using Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PCL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public Get Getter;
+        public ValuesController()
+        {
+            this.Getter = new Get();
+        }
+        
+        // GET api/values
+        [HttpGet]
+        public ActionResult<Hello> Get()
+        {
+            return Getter.GetHello();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "hej";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
